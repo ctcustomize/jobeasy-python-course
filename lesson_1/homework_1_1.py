3dc53d1d30dde762337623da11cceb17e7a2c9b1
c.z <75642426+ctcustomize@users.noreply.github.com>
2020-12-09 11:24:31 -0800

    dsd

--- lesson_1/homework_1_1.py
+++ lesson_1/homework_1_1.py
@@ -2,52 +2,46 @@
 
 a = 10
 b = 15
-result_sum = None
-
+print(a+b)
 
 # Find the difference between c and d. Save the expression to the result_diff variable
 
 c = 10
 d = 15
-result_diff = None
-
+print(c-d)
 
 # Multiply e by f. Save the expression to the result_multiplication variable
 
 e = 10
 f = 15
-result_multiplication = None
-
+print(e*f)
 
 # Divide g by h. Save the expression to the result_division variable
 
 g = 10
 h = 15
-result_division = None
+print(g/h)
 
 
 # Calculate the floored division result of i and j. Save the expression to the result_division_floored variable
 
 i = 10
 j = 15
-result_division_floored = None
-
+print(i//j)
 
 # Calculate the remainder of k divided by l. Save expression to the result_division_remainder variable
 
 k = 10
 l = 15
-result_division_remainder = None
-
+print(k%l)
 
 # Rise m to the power n. Save the expression to the result_power variable
 
 m = 5
 n = 2
-result_power = None
-
+print(m**n)
 
 # Convert an o variable to negative. Save the expression to the result_negative variable
 
 o = 100
-result_negative = None
+print(-o)
