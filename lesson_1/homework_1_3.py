a6762df183b452c8be9e6a3d172eb05360d200d4
c.z <75642426+ctcustomize@users.noreply.github.com>
2020-12-09 13:37:38 -0800

    updated

--- lesson_1/homework_1_3.py
+++ lesson_1/homework_1_3.py
@@ -4,14 +4,14 @@ import math
 # Round a off to three decimal places.
 
 a = 10.04789
-round(a[,3])
-
+print(round(a,2))
 # Round up result_2 to greater value.
 
 result_2 = 5 / 2 * 6 + 1.25 - 4
-
+print(math.floor(result_2))
 
 # Round up result_3 to lesser value.
 
 result_3 = 8 / 3 * 5 + 4.75 - 7
+print(math.ceil(result_3))
 
